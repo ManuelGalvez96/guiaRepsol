a51e7964e54deddbb594b4e8458a8485d6387720
SergiNebot <440244.joan23@fje.edu>
2026-02-09 19:33:18 +0100

    modificamos los seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'rol',
+        'restaurante_id',
     ];
 
     /**
@@ -44,5 +46,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the restaurante that the user is associated with.
+     */
+    public function restaurante()
+    {
+        return $this->belongsTo(Restaurante::class);
     }
 }
