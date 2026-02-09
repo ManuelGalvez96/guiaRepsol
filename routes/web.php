f264e8c2c6afd2e2ddb173f9e10c957e2fb57bcd
SergiNebot <440244.joan23@fje.edu>
2026-02-09 18:14:07 +0100

    subimos los seeders

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\AuthUser;
-=======
 use App\Http\Controllers\RestauranteController;
->>>>>>> 47bf0a336164b4a41f0942c4d8ca5cbfe446a010
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-<<<<<<< HEAD
 // Rutas de autenticación
 Route::get('/login', [AuthUser::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthUser::class, 'login'])->name('login.post');
@@ -19,7 +15,6 @@ Route::post('/logout', [AuthUser::class, 'logout'])->name('logout');
 
 Route::get('/register', [AuthUser::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthUser::class, 'register'])->name('register.post');
-=======
 Route::view('/login', 'log.login')->name('login');
 
 // Ruta protegida - Solo accesible para usuarios autenticados (Descomentar cuando se implemente autenticación)
@@ -30,4 +25,3 @@ Route::view('/login', 'log.login')->name('login');
 // Ruta temporal sin protección para desarrollo (Eliminar después de implementar autenticación)
 Route::get('/restaurantes', [RestauranteController::class, 'index'])->name('restaurantes');
 
->>>>>>> 47bf0a336164b4a41f0942c4d8ca5cbfe446a010
